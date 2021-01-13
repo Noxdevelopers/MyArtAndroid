79a36cb7c830a7756052fc9ca78bf736a6d1f4be
Amir Reza <amirrezaghahremani@gmail.com>
2021-01-13 22:43:09 +0330

    24/10/99 22:42 amir

--- app/src/main/java/com/example/myart/MainActivity.java
+++ app/src/main/java/com/example/myart/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.myart;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gauravk.bubblenavigation.BubbleNavigationConstraintView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    BubbleNavigationConstraintView top_navigation_constraint ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
+    }
+
+    private void init() {
 
     }
 }
